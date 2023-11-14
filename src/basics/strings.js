2521cc0fbe096725a0720a91f4f6a50f7a7c6fc3
Rignchen <rinchendemort@gmail.com>
2023-11-14 15:57:27 +0100

    function to replace word keeping case

--- src/basics/strings.js
+++ src/basics/strings.js
@@ -6,6 +6,28 @@
  * @param {string} newWord
  * @return {string} true if n is bigger than 2
  */
+
 export function findAndReplacePreservingCase(needle, haystack, newWord) {
-  //
+  if (typeof needle !== "string" || typeof haystack !== "string" || typeof newWord !== "string") {
+    throw "Unsupported types";
+  }
+  let regex = new RegExp(needle, "gi");
+  return haystack.replaceAll(regex, function (match) {
+    return match.split("").map(function (char, index) {
+      if (index > newWord.length - 1) {
+        return "";
+      } else if (char === char.toUpperCase()) {
+        return newWord[index].toUpperCase();
+      } else {
+        return newWord[index].toLowerCase();
+      }
+    }).reduce((a,b) => a+b);
+  });
 }
+
+/*
+>>> print(f"A-Z = {ord('A')}-{ord('Z')}")
+A-Z = 65-90
+>>> print(f"a-z = {ord('a')}-{ord('z')}")
+a-z = 97-122
+*/
